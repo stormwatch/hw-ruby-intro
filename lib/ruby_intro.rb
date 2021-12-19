3ddc16ee80bf43eae1e33d45d985238734dcf62c
Ezequiel Birman <ebirman77@gmail.com>
2021-12-19 01:07:54 -0300

    Solve homework excercises

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,51 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  arr.sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  arr.max(2).sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  h = {}
+  arr.any? do |x|
+    complement = h[n - x]
+    h[x] = true
+    complement
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  /\A[a-z&&[^aeiou]]/i.match?(s)
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  s == '0' || /^[01]*00$/.match(s)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'ISBN cannot be blank' if isbn == ''
+    raise ArgumentError, 'price must be positive' if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', price)
+  end
 end
